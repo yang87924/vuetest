20742ff1ae400bcbd18500e1e959bb393c9e9163
yang <yang87924@gmail.com>
2023-07-12 10:43:16 +0800

    Initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>About Us</h1>
+        <p>This is the About Us page content.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'About',
+}
+</script>
+
+<style>
+h1 {
+    color: green;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the home page content.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+}
+</script>
+
+<style>
+h1 {
+    color: blue;
+}
+</style>
